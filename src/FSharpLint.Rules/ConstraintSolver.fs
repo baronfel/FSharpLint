80be988001fb17dd4422d6c695466321c817638b
Matt <matt@mattmcveigh.co.uk>
2015-07-02 19:57:20 +0100

    Started looking at a SMT visitor

---
+++ src/FSharpLint.Rules/ConstraintSolver.fs
@@ -0,0 +1,104 @@
+﻿(*
+    FSharpLint, a linter for F#.
+    Copyright (C) 2014 Matthew Mcveigh
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*)
+
+namespace FSharpLint.Rules
+
+module ConstraintSolver =
+    
+    open Microsoft.FSharp.Compiler.Ast
+    open Microsoft.FSharp.Compiler.Range
+    open Microsoft.FSharp.Compiler.SourceCodeServices
+    open Microsoft.Z3
+    open FSharpLint.Framework.Ast
+    open FSharpLint.Framework.Configuration
+    open FSharpLint.Framework.LoadVisitors
+
+    type Scope = System.Collections.Generic.Dictionary<string, int>
+
+    type Scopes = Scope list
+
+    type SymbolTable private (parentScopes:Scopes) = 
+        let scopes = Scope()::parentScopes
+
+        let scopeLevel = List.length scopes
+
+        let generateSymbol identifier numberOfLocalIdentsShadowed scopeLevel =
+            sprintf "%s_%d_%d" identifier numberOfLocalIdentsShadowed scopeLevel
+
+        let numberOfLocalDeclarations (scope:Scope) identifier =
+            match scope.TryGetValue identifier with
+                | (true, numberOfIdentsShadowed) -> numberOfIdentsShadowed
+                | (false, _) -> 0
+
+        member this.GetSymbol(identifier) =
+            let rec getSymbol = function
+                | (scope:Scope)::parentScopes ->
+                    let numLocalDecl = numberOfLocalDeclarations scope identifier
+
+                    if numLocalDecl > 0 then
+                        let scopeLevel = List.length parentScopes + 1
+                        Some(generateSymbol identifier numLocalDecl scopeLevel)
+                    else
+                        getSymbol parentScopes
+                | [] -> None
+
+            getSymbol scopes
+
+        member this.AddDeclaration(identifier) =
+            match scopes with
+                | scope::_ -> 
+                    let numLocalDecl = numberOfLocalDeclarations scope identifier
+                
+                    if numLocalDecl > 0 then
+                        scope.[identifier] <- numLocalDecl + 1
+                    else
+                        scope.Add(identifier, 1)
+                        
+                    generateSymbol identifier (numLocalDecl + 1) scopeLevel
+                | [] -> 
+                    System.InvalidOperationException("There were no scopes in the symbol table.")
+                        |> raise
+
+        member this.NewChildScope() =
+            SymbolTable(scopes)
+
+        static member NewRootScopeSymbolTable() = 
+            SymbolTable([])
+
+    [<Literal>]
+    let AnalyserName = "ConstraintSolver"
+
+    let isRuleEnabled config ruleName =
+        isRuleEnabled config AnalyserName ruleName |> Option.isSome
+
+    let rec visitor (context:Context) visitorInfo checkFile (astNode:CurrentNode) = 
+        Continue
+
+    let initialVisitor visitorInfo checkFile (astNode:CurrentNode) = 
+        use context = new Context()
+        ContinueWithVisitor (visitor context visitorInfo checkFile)
+
+    type RegisterBindingVisitor() = 
+        let plugin =
+            {
+                Name = AnalyserName
+                Visitor = Ast(initialVisitor)
+            }
+
+        interface IRegisterPlugin with
+            member this.RegisterPlugin with get() = plugin
---
+++ tests/FSharpLint.Rules.Tests/TestConstraintSolver.fs
@@ -0,0 +1,96 @@
+﻿(*
+    FSharpLint, a linter for F#.
+    Copyright (C) 2014 Matthew Mcveigh
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*)
+
+module TestConstraintSolver
+
+open NUnit.Framework
+open FSharpLint.Rules.ConstraintSolver
+open FSharpLint.Framework.Configuration
+open FSharpLint.Framework.LoadVisitors
+
+let config = 
+    Map.ofList 
+        [ 
+            (AnalyserName, 
+                { 
+                    Rules = Map.ofList []
+                    Settings = Map.ofList []
+                }) 
+        ]
+
+[<TestFixture>]
+type TestConstraintSolverRules() =
+    inherit TestRuleBase.TestRuleBase(Ast(initialVisitor), config)
+
+    [<Test>]
+    member this.TestAddSymbol() = 
+        let symbolTable = SymbolTable.NewRootScopeSymbolTable()
+
+        let symbol = symbolTable.AddDeclaration "ident"
+
+        Assert.AreEqual("ident_1_1", symbol)
+
+    [<Test>]
+    member this.TestAddShadowedSymbol() = 
+        let symbolTable = SymbolTable.NewRootScopeSymbolTable()
+
+        let symbol = symbolTable.AddDeclaration "ident"
+
+        let shadowingSymbol = symbolTable.AddDeclaration "ident"
+
+        Assert.AreEqual("ident_2_1", shadowingSymbol)
+
+    [<Test>]
+    member this.TestAddSymbolToChildScope() = 
+        let symbolTable = SymbolTable.NewRootScopeSymbolTable()
+
+        let childScope = symbolTable.NewChildScope()
+
+        childScope.AddDeclaration "ident" |> ignore
+
+        Assert.AreEqual(Some("ident_1_2"), childScope.GetSymbol "ident")
+
+    [<Test>]
+    member this.TestGetSymbolInParentScope() = 
+        let symbolTable = SymbolTable.NewRootScopeSymbolTable()
+
+        symbolTable.AddDeclaration "ident" |> ignore
+
+        let childScope = symbolTable.NewChildScope()
+
+        Assert.AreEqual(Some("ident_1_1"), childScope.GetSymbol "ident")
+
+    [<Test>]
+    member this.TestGetSymbolInNoScope() = 
+        let symbolTable = SymbolTable.NewRootScopeSymbolTable()
+
+        let symbol = symbolTable.GetSymbol "ident"
+
+        Option.isNone symbol |> Assert.IsTrue
+
+    [<Test>]
+    member this.TestSymbolInChildScopeCannotBeAccessedInParentSymbolTable() = 
+        let symbolTable = SymbolTable.NewRootScopeSymbolTable()
+
+        let childScope = symbolTable.NewChildScope()
+
+        childScope.AddDeclaration "ident" |> ignore
+
+        let symbol = symbolTable.GetSymbol "ident"
+
+        Option.isNone symbol |> Assert.IsTrue
